cba7078691179fa6f5b6ab126ae0d7f20d37e31f
Ryan C <ryanchng.ps@gmail.com>
2021-07-15 03:24:59 +0800

    rearrange column and other fix 4 digit postcode with leading 0

--- rFoodBank/airtable/airtable_pandas.py
+++ rFoodBank/airtable/airtable_pandas.py
@@ -5,13 +5,12 @@ from dotenv import load_dotenv
 import pandas as pd
 from datetime import datetime
 
-
 load_dotenv()
 
 pd.set_option('display.width', 980)
 pd.set_option('display.max_colwidth', 180)
 pd.set_option('display.max_rows', 3000)
-pd.set_option('display.max_column', 200)
+pd.set_option('display.max_column', 50)
 pd.set_option('display.float_format', '{:,.2f}'.format)
 
 AIRTABLE_API_KEY = os.environ.get('AIRTABLE_API_KEY')
@@ -20,7 +19,6 @@ S3_ACCESS_KEY = os.environ.get('S3_ACCESS_KEY')
 S3_SECRET = os.environ.get('S3_SECRET')
 AIRTABLE_TABLE_NAME = 'Resources'
 AIRTABLE_VIEW = 'All Data (by username)'
-
 
 today = datetime.today()
 Day = f"{today.strftime('%d')}"
@@ -33,16 +31,29 @@ io_key = f'rMsia_{Day}{Month}{Year}_{Time}'
 json_out = os.path.join(os.path.expanduser('~'), 'PycharmProjects', 'rMalaysiaFoodBank', 'rFoodBank', 'airtable',
                         'pandas.json')
 
-
 data = airtable.connect(AIRTABLE_API_KEY, AIRTABLE_BASE_ID, AIRTABLE_TABLE_NAME).get(view=AIRTABLE_VIEW)
 df = pd.DataFrame(data)
-print(df.shape)
+
+df = df[
+    ['Resource Name', 'Organisation Name', 'Imported by User', 'Launch URL', 'Launch URL Language', 'Launch URL Date',
+     'Resources Offered', 'Access Method', 'Must Contact First', 'Freeform Notes Misc', 'Operating Hours',
+     'Government Funded', 'Religious', 'Quantity Limits', 'Resource End Date', 'Any Other Limits?',
+     'Resource Freeform Notes', 'Access Method Other', 'Covers Area?', 'Freeform Address', 'Postcode', 'State/WP',
+     'City', 'GPS Plus Code', 'GPS Latitude', 'GPS Longitude', 'Other Contact Details', 'Contact Method(s)',
+     'Contact Number(s)', 'Contact Name', 'Contact Freeform Info', 'Resources Offered Other',
+     'Latest Social Media Post URL', 'Latest Social Media Post Date', 'Estimated Supply Levels',
+     'Donations Bank Info', 'Donations Accepted', 'Donations Accepted Other', 'Inactive?', 'Other Limits Details',
+     'Launch Freeform Text', 'Info Source', 'Unique Identifier(RL)', 'Import Date']]  # Religious Other not in due to empty column
 
 df.reset_index(drop=True, inplace=True)
-df.drop(['createdTime'], axis=1, inplace=True)
 
-# df.to_json(json_out, orient='split', date_format='iso')
+print(df.shape)
+print(df.head(5))
+print(df.columns)
 
+
+
+df.to_json(json_out, orient='records', date_format='iso')
 
 # """ AWS Part """
 # client = boto3.client(
